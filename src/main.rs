47c3fa1c8ca1ed985a5f3d1aa7c764ea9ff9383f
taylor d <taylor.delph@eyecuelab.com>
2022-04-07 16:57:29 -0700

    returning values from loops

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
+// fn main() {
+//   let mut count = 0;
+//   'counting_up: loop {
+//       println!("count = {}", count);
+//       let mut remaining = 10;
+
+//       loop {
+//           println!("remaining = {}", remaining);
+//           if remaining == 9 {
+//               break;
+//           }
+//           if count == 2 {
+//               break 'counting_up;
+//           }
+//           remaining -= 1;
+//       }
+
+//       count += 1;
+//   }
+//   println!("End count = {}", count);
+// }
+
+
+// Feature a loop to retry an operation you know might fail, such as checking whether a thread has completed its job. You might also need to pass the result of that operation out of the loop to the rest of your code. 
 fn main() {
-  let mut count = 0;
-  'counting_up: loop {
-      println!("count = {}", count);
-      let mut remaining = 10;
+  let mut counter = 0;
 
-      loop {
-          println!("remaining = {}", remaining);
-          if remaining == 9 {
-              break;
-          }
-          if count == 2 {
-              break 'counting_up;
-          }
-          remaining -= 1;
-      }
+  let result = loop {
+    counter += 1;
 
-      count += 1;
-  }
-  println!("End count = {}", count);
+    if counter == 10 {
+      break counter * 2;
+    }
+  };
+
+  println!("the result is {}", result);
 }
